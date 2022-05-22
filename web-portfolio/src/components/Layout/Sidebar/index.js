efac726098e50bb612aa3f000b1ccea7c40ff23c
Brian Fernando <brianfdo@users.noreply.github.com>
2022-05-22 14:58:08 -0400

    started navbar

--- web-portfolio/src/components/Layout/Sidebar/index.js
+++ web-portfolio/src/components/Layout/Sidebar/index.js
@@ -1,29 +1,29 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import Logo from '../../../assets/images/elmo.png'
+import Elmo from '../../../assets/images/elmo.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = function () {
+const Sidebar = () => (
     <div className='nav-bar'>
         <Link className="logo" to="/">
-            <img src={Logo} alt="logo"/>
+            {/* <img src={Elmo} alt="logo"/> */}
             {/* <img src={LogoSubtitle} alt="fern"/> */}
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
 
         </nav>
     </div>
-}
+)
 
 export default Sidebar
--- web-portfolio/src/components/Layout/index.js
+++ web-portfolio/src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import Sidebar from './Sidebar'
 
-const Layout = function () {
+const Layout = () => {
     return <><Sidebar/></>
 }
 
