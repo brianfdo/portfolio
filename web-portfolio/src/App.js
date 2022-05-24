a31fc0f2b95764df5526ea214d8bd5a28aaa8580
Brian Fernando <brianfdo@users.noreply.github.com>
2022-05-24 01:36:05 -0400

    hask link and aos

--- web-portfolio/src/App.js
+++ web-portfolio/src/App.js
@@ -1,13 +1,30 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react"; 
+
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      once: true,
+      // useClassNames: true,
+      // initClassName: false,
+      animatedClassName: 'animated',
+    });
+  });
   return (
     <>
-    <Routes>
+    {/* <Routes>
       <Route path="/" element={<Layout />}/>
-    </Routes>
+    </Routes> */}
+    <div data-aos="fade-right"
+         data-aos-easing="ease-in-out"
+         data-aos_duration="10000">
+      <Layout data-aos="fade-right"/>
+    </div>
     </>
 
   );
--- web-portfolio/src/components/Layout/Sidebar/index.js
+++ web-portfolio/src/components/Layout/Sidebar/index.js
@@ -1,4 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 import './index.scss'
 import Elmo from '../../../assets/images/elmo.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
@@ -8,17 +9,17 @@ import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className="logo" to="/">
-            {/* <img src={Elmo} alt="logo"/> */}
+            <img src={Elmo} alt="logo"/>
             {/* <img src={LogoSubtitle} alt="fern"/> */}
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <HashLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            </HashLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/#about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/#contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
 
